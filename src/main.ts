8a2542edc5c330b14c81cb6e8d31c0ec998f23ae
Juan Marcelo Tisot <marcelotisot1990@gmail.com>
2025-05-08 11:10:06 -0300

    se configuran variables de entorno

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { envs } from './config';
 
 async function bootstrap() {
+  const logger = new Logger('Main-Gateway');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix('api');
+
+  await app.listen(envs.port);
+
+  logger.log(`Client Gateway running on port ${envs.port}`);
 }
 bootstrap();
